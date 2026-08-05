caaff29f7c0f2edf37c4016d58719e98c54b442c
Caio Gustavo <caiogggustavo49@gmail.com>
2026-08-05 14:47:06 -0300

    feat: service layer, component registration

---
+++ course/src/main/java/com/workshop/course/services/UserService.java
@@ -0,0 +1,22 @@
+package com.workshop.course.services;
+
+import com.workshop.course.entities.User;
+import com.workshop.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+    public User findById(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+}
