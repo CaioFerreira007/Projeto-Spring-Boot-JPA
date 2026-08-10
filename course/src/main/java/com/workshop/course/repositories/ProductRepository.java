4000b1f75effc27cfcc83efd6829b6d713355cad
Caio Gustavo <caiogggustavo49@gmail.com>
2026-08-10 13:31:34 -0300

    feat:class product created

---
+++ course/src/main/java/com/workshop/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.workshop.course.repositories;
+
+import com.workshop.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
