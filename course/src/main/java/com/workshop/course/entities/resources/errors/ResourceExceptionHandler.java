6059d928b2425a74ad61490195bd31185d262be8
Caio Gustavo <caiogggustavo49@gmail.com>
2026-08-13 17:49:30 -0300

    feat: exception handling - findById

---
+++ course/src/main/java/com/workshop/course/entities/resources/errors/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.workshop.course.entities.resources.errors;
+
+import com.workshop.course.services.middlewares.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandartError> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        String error = "Resource not found: ";
+        HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+        StandartError standartError = new StandartError(Instant.now(), httpStatus.value(), error, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(httpStatus).body(standartError);
+    }
+}
--- course/src/main/java/com/workshop/course/services/UserService.java
+++ course/src/main/java/com/workshop/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.workshop.course.services;
 
 import com.workshop.course.entities.User;
 import com.workshop.course.repositories.UserRepository;
+import com.workshop.course.services.middlewares.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
